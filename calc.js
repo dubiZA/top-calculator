f49e95d22711de0ec553f6fbbd3ee26dd79e7379
dubiZA <darius@dubiza.io>
2022-12-13 14:47:06 -0700

    Create new storeOperationNumber()
    The storeOperationNumber function is used to make sure a valid
    sequence of numbers is being input to the calculator. 0 as the only
    digit should not be allowed. Struggled with handling the logic for
    this so it's messy right now - will need to look making it more
    concise when my brain is working better again :).
    
    In creating the storeOperationsNumber function, most of the code
    was removed from the renderDisplay function to the new store function.
    renderDisplay now will only do just that.

--- calc.js
+++ calc.js
@@ -4,7 +4,7 @@ const buttons = document.querySelectorAll("button");
 buttons.forEach(function(button) {
   button.addEventListener("click", function() {
     if (button.dataset.value) {
-      renderDisplay(button.dataset.value);
+      storeOperationNumber(button.dataset.value);
     }
     if (button.id === "clear") {
       resetDisplay();
@@ -12,16 +12,27 @@ buttons.forEach(function(button) {
   });
 });
 
+function storeOperationNumber(value) {
+  if (value !== "0") {
+    if (operationNumber.length < 9) {
+      operationNumber += value;
+      renderDisplay(operationNumber);
+    }
+  } else if (value === "0" && operationNumber !== "") {
+    if (operationNumber.length < 9) {
+      operationNumber += value;
+      renderDisplay(operationNumber);
+    }
+  }
+}
+
 function renderDisplay(value) {
   const displayDiv = document.querySelector(".display-row");
-  if (operationNumber.length < 9 && operationNumber !== "0") {
-    operationNumber += value;
-    displayDiv.textContent = operationNumber;
-  }
+  displayDiv.textContent = value;
 }
 
 function resetDisplay() {
   const displayDiv = document.querySelector(".display-row");
-  displayDiv.textContent = 0;
+  displayDiv.textContent = "0";
   operationNumber = "";
 }
